e29357bc735d3d3512a8ce567a6577cc8323078a
toriwat328 <toriwat328@gmail.com>
2020-02-29 13:09:35 -0500

    updated with redux boilerplate with types, actions reducer and store

---
+++ client/src/actions/stockActions.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { GET_STOCK, BUY_STOCK } from './types';
+
+export const getStocks = () => dispatch => {
+    axios.get('/stocks')
+        .then(res => dispatch({
+            type: GET_STOCK,
+            payload: res.data
+        }))
+        .catch(err => console.log(err))
+}
+
+export const buyStocks = (stock) => dispatch => {
+    axios.get('/stocks')
+        .then(res => dispatch({
+            type: BUY_STOCK,
+            payload: res.data
+        }))
+        .catch(err => console.log(err))
+}
---
+++ client/src/actions/types.js
@@ -0,0 +1,2 @@
+export const GET_STOCK = 'GET_STOCK';
+export const BUY_STOCK = 'BUY_STOCK';
