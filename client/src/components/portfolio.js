95a5b2c2cbe3de6dd9f0de4e5d99499ceefbcc42
toriwat328 <toriwat328@gmail.com>
2020-03-01 17:43:13 -0500

    trying to get stocks loaded before i call api to get current price

--- client/src/components/portfolio.js
+++ client/src/components/portfolio.js
@@ -5,9 +5,24 @@ import { connect } from 'react-redux';
 import { getStocks } from '../actions/stockActions';
 import PropTypes from 'prop-types';
 import BuyForm from './buystocksform';
+require('dotenv').config()
+
+const apikey = process.env.REACT_APP_IEXAPI;
 
 
 class Portfolio extends Component {
+    state = {
+        baseURL: 'https://cloud.iexapis.com/',
+        version: 'stable/',
+        endpoint: 'stock/',
+        symbol: [],
+        query: '/quote?token=' + apikey,
+        searchURL: '',
+        latestPrice: '',
+        open: ''
+
+    }
+
     static propTypes = {
         getStocks: PropTypes.func.isRequired,
         stock: PropTypes.object.isRequired
@@ -19,58 +34,87 @@ class Portfolio extends Component {
 
 
 
-
-
     }
 
+    // componentDidUpdate(){
+    //
+    //     console.log(this.makeUnique(this.props.stock));
+    //
+    // }
+
+
+
+
+
     makeUnique = (stocks) => {
-        //create empty object variables
-        let uniqueSymbols = {};
-
-        // stocks.map(({symbol, qtyshares})=> {
-        //     if(uniqueSymbols[symbol]){
-        //         uniqueSymbols[symbol] += qtyshares
-        //     }else {
-        //         uniqueSymbols[symbol] = qtyshares
-        //     }
-        //     return uniqueSymbols;
-        // });
 
 
+
+
+        const uniqueSymbols = {};
+        // //create empty object variables
+        // //loop through stocks array if symbol is already in new obj, then add qty to symbol value, otherwise add symbol with 0 as value
         for(let i = 0; i <= stocks.stocks.length-1; i++){
-            if(uniqueSymbols[stocks.stocks[i].symbol]){
-                console.log(uniqueSymbols[stocks.stocks[i].symbol]);
-                uniqueSymbols[stocks.stocks[i].symbol] += stocks.stocks[i].qtyshares
+            if(!uniqueSymbols[stocks.stocks[i].symbol]){
+                uniqueSymbols[stocks.stocks[i].symbol] = {};
+                uniqueSymbols[stocks.stocks[i].symbol].shares = stocks.stocks[i].qtyshares;
+
             }else {
-                console.log(uniqueSymbols[stocks.stocks[i].symbol]);
-                uniqueSymbols[stocks.stocks[i].symbol] = stocks.stocks[i].qtyshares
+                uniqueSymbols[stocks.stocks[i].symbol].shares += stocks.stocks[i].qtyshares
+
             }
+
+
         }
+
+        // for(let symbols in uniqueSymbols){
+        //     console.log(symbols);
+        //     this.setState({
+        //         symbol: symbols
+        //     })
+        //
+        //     if(!uniqueSymbols[symbols].latestPrice){
+        //             this.setState({
+        //                 searchURL: this.state.baseURL + this.state.version + this.state.endpoint + this.state.symbol + this.state.query
+        //             }, () => {
+        //                 fetch(this.state.searchURL)
+        //                     .then(response => {
+        //                         return response.json()
+        //                     }).then(json => this.setState({
+        //                         latestPrice: json.latestPrice,
+        //                         open: json.open
+        //                     }, () => {
+        //                          uniqueSymbols[symbols].latestPrice = this.state.latestPrice;
+        //                          uniqueSymbols[symbols].open = this.state.open;
+        //                     })
+        //
+        //             )}, err => console.log(err))
+        //
+        //         }
+        // }
+
+        // console.log(this.state.latestPrice);
 
         return uniqueSymbols;
 
-        //loop through stocks array if symbol is already in new obj, then add qty to symbol value, otherwise add symbol with 0 as value
     }
+
+
 
 
 
 
     render(){
         const { stocks } = this.props.stock;
-        console.log(this.makeUnique({ stocks }));
+        console.log(this.props.stock);
+        console.log(this.makeUnique(this.props.stock));
+
         return (
             <Row>
             <Col sm={7}>
             <h2>Portfolio ($5943.34)</h2>
             <Container
                 style={{borderRight: '.5px solid grey', height: '700px'}}>
-                <ListGroup>
-                {stocks.map(({_id, symbol, qtyshares})=> (
-                    <ListGroupItem>
-                        { symbol } - {qtyshares} Shares $5000
-                    </ListGroupItem>
-                ))}
-                </ListGroup>
             </Container>
             </Col>
             <BuyForm />
